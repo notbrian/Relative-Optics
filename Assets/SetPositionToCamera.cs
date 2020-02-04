dabc89b431f9961d05288b56a8f161d25095cf9d
Brian Nguyen <nguyen.brian70@gmail.com>
2020-02-04 13:29:48 -0500

    Add bloom and render texture and stuff

---
+++ Assets/SetPositionToCamera.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetPositionToCamera : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+         Camera cam = GameObject.Find("Main Camera").GetComponent<Camera>();
+         transform.SetPositionAndRotation(cam.transform.position, transform.rotation);
+    }
+}
