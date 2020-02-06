6f9a7ce6a22e495852865de21325c4e0e2e509b1
Michael Shefer <shefermichael@gmail.com>
2020-02-05 21:02:52 -0500

    alpha transition

---
+++ Assets/Scripts/alphaTransition.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class alphaTransition : MonoBehaviour
+{
+    bool alpha;
+    // Start is called before the first frame update
+    void Start()
+    {
+        alpha = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+
+        if (alpha)
+        {
+            GetComponent<Renderer>().enabled = false;
+            alpha = false;
+            Debug.Log("alpha is off");
+        }
+
+        else if(Input.GetKeyDown(KeyCode.Space))
+
+        if(!alpha)
+        {
+            GetComponent<Renderer>().enabled = true;
+            alpha = true;
+            Debug.Log("alpha is on");
+        }
+
+    }
+}
