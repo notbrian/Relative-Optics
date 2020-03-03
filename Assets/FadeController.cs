cfa1a9a7646f033e3bee6a422ad73186f165e928
Brian Nguyen <nguyen.brian70@gmail.com>
2020-03-03 09:59:18 -0500

    Add fungus and stuff

---
+++ Assets/FadeController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DigitalRuby.Tween;
+namespace DigitalRuby.Tween
+{
+
+
+    public class FadeController : MonoBehaviour
+    {
+        public Material material;
+
+
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        public void fadeOutSphere()
+        {
+
+            System.Action<ITween<Color>> updateColor = (t) =>
+                  {
+                      Debug.Log("Tweening");
+                      Debug.Log(t.CurrentValue);
+                      material.SetColor("_BaseColor", t.CurrentValue);
+                  };
+            TweenFactory.Tween("Base Map", new Color(1f, 1f, 1f, 1f), new Color(1f, 1f, 1f, 0f), 1.0f, TweenScaleFunctions.QuadraticEaseOut, updateColor);
+
+        }
+
+        public void fadeInSphere()
+        {
+
+            System.Action<ITween<Color>> updateColor = (t) =>
+                  {
+                      Debug.Log("Tweening");
+                      Debug.Log(t.CurrentValue);
+                      material.SetColor("_BaseColor", t.CurrentValue);
+                  };
+            TweenFactory.Tween("Base Map", new Color(1f, 1f, 1f, 0f), new Color(1f, 1f, 1f, 1f), 1.0f, TweenScaleFunctions.QuadraticEaseOut, updateColor);
+
+        }
+    }
+}
---
+++ Assets/Scripts/ChangeVideoURL.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeVideoURL : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public UnityEngine.Video.VideoPlayer VideoP;
+
+    void Start()
+    {
+        VideoP = GetComponent<UnityEngine.Video.VideoPlayer>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void changeVideoURL(string URL)
+    {
+        VideoP.url = URL;
+    }
+}
