a4b2d922044094873f17ca9e86f2f4a6af987afe
Brian Nguyen <nguyen.brian70@gmail.com>
2020-03-12 13:05:06 -0400

    Add non steam vr framework

---
+++ Assets/ButtonCallFungus.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Fungus;
+public class ButtonCallFungus : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public string FungusBlock;
+    public Flowchart flowchart;
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void CallFungus()
+    {
+        flowchart.ExecuteBlock(FungusBlock);
+
+    }
+}
