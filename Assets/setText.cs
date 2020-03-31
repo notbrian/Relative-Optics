263a6ea300de0787e591a84dfb815a01c6710249
Brian Nguyen <nguyen.brian70@gmail.com>
2020-03-31 15:46:41 -0400

    Cleanup add Node prefab

---
+++ Assets/setText.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[ExecuteInEditMode]
+public class setText : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private GameObject[] TextLabels = new GameObject[4];
+    public string ActionName = "";
+    void Start()
+    {
+        Transform Canvas = this.gameObject.transform.GetChild(0);
+        int length = this.gameObject.transform.GetChild(0).childCount;
+
+        for (int i = 0; i < length; i++)
+        {
+            TextLabels[i] = Canvas.GetChild(i).gameObject;
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int length = this.gameObject.transform.GetChild(0).childCount;
+        for (int i = 0; i < length; i++)
+        {
+            TextLabels[i].GetComponent<Text>().text = ActionName;
+        }
+    }
+}
