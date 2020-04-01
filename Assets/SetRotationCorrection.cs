87e43612432edb099ddcbb0f82f558b944879146
Brian Nguyen <nguyen.brian70@gmail.com>
2020-03-31 20:25:54 -0400

    Add new videos and redo RC Car block

---
+++ Assets/SetRotationCorrection.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetRotationCorrection : MonoBehaviour
+{
+
+    public void setYRotation(int yRot)
+    {
+        Vector3 newRotation = transform.eulerAngles;
+        newRotation.y = yRot;
+        transform.eulerAngles = newRotation;
+    }
+}
